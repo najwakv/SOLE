f86dd82dea155aa5e82c94f8510020c3c7a48ce9
najwa <najwa.kv20@gmail.com>
2023-02-07 20:12:21 +0530

    bugs fixed

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -476,7 +476,7 @@ module.exports = {
     //View order
     allOrder: async(req,res) =>{
         const orders = await OrderModel.find({});
-        console.log(orders.items);
+        console.log(orders);
         res.render('admin/order', { orders, index: 1, admin: req.session.admin })
     },
     //Order status
